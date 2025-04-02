108d7000bc0e57c2b77f26c633e706443ab135d6
aivoiapp <aivoiapp.adm@gmail.com>
2025-04-02 06:55:18 -0500

    fix pagina en blanco

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // Usar rutas relativas en lugar de absolutas
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
